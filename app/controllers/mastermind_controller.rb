ff12af5eebcac7dfaa130b06993afafa6a6c9f5e
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-05-21 19:20:06 -0400

    updated controller spec and implementation

--- app/controllers/mastermind_controller.rb
+++ app/controllers/mastermind_controller.rb
@@ -2,9 +2,10 @@ class MastermindController < ApplicationController
   before_filter :authenticate_user!, :only => [:dashboard]
 
   def index
-    render :action => "dashboard" if user_signed_in?
-  end
-
-  def dashboard
+    if user_signed_in?
+      @user = current_user
+      @leaders = User.find_top_ten
+      render :action => "dashboard"
+    end
   end
 end
--- spec/controllers/mastermind_controller_spec.rb
+++ spec/controllers/mastermind_controller_spec.rb
@@ -1,33 +1,42 @@
 require 'spec_helper'
 
 describe MastermindController do
-  include Devise::TestHelpers
-
   describe "GET 'index'" do
-    context "new user" do
+    context "when user is not logged in" do
       it "renders anonymous home page" do
         get :index
         response.should render_template("index")
       end
     end
 
-    context "authenticated user" do
-      let (:user) { mock_model(User).as_null_object }
+    context "when user is logged in" do
+      let(:user) { mock_model(User).as_null_object }
+      let(:leaders) { [] }
 
       before(:each) do
-        authenticate user
+        log_in(user)
+        User.stub(:find_top_ten).and_return(leaders)
+        get :index
+      end
+
+      it "assigns @user" do
+        assigns[:user].should eq(user)
+      end
+
+      it "assigns @leaders" do
+        assigns[:leaders].should eq(leaders)
       end
 
       it "renders logged in home page" do
-        get :index
         response.should render_template("dashboard")
       end
     end
   end
 end
 
-def authenticate(user)
+def log_in(user)
   # mock up an authentication in the underlying warden library
   request.env['warden'] = mock(Warden, :authenticate => user,
-                                       :authenticate! => user)
+                                       :authenticate! => user,
+                                       :user => user)
 end
