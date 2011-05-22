5be4ebb88410f013da95403774fd51145a2c7a52
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-05-22 11:00:32 -0400

    update migration comment

--- db/migrate/20110521222610_add_game_and_guess_counts_to_user.rb
+++ db/migrate/20110521222610_add_game_and_guess_counts_to_user.rb
@@ -4,8 +4,7 @@ class AddGameAndGuessCountsToUser < ActiveRecord::Migration
     add_column :users, :games_lost, :integer, :default => 0
     add_column :users, :average_guess_count, :decimal, :default => 0
 
-    # we need only the games_won index for the leader board, but others might be useful
-    # in future
+    # these columns are used to sort leader board
     add_index :users, :games_won
     add_index :users, :games_lost
     add_index :users, :average_guess_count
