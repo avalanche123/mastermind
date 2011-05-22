32ac8dbd90c6fa230bbfb1b1e57a95c074d9de5f
Bulat Shakirzyanov <mallluhuct@gmail.com>
2011-05-22 01:54:23 -0400

    spec for GameController#create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,8 @@
 class GamesController < ApplicationController
+  before_filter :authenticate_user!
+
   def create
+    current_user.play
   end
 
   def show
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 describe GamesController do
+  let(:user) { mock_model(User).as_null_object }
+  before(:each) { log_in user }
 
   describe "POST 'create'" do
-    it "should be successful"
+    it "creates a new game" do
+      user.should_receive(:play)
+      post :create
+    end
+
+    it "saves a new game"
   end
 
   describe "GET 'show'" do
